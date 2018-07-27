f1e6cd36295912083f4ae26e05a93e5b40ba24bc
Pavel <CrazyAlarmClock@gmail.com>
2018-07-27 23:59:29 +0300

    ++

--- netology_py/2.1/Main.py
+++ netology_py/2.1/Main.py
@@ -4,22 +4,19 @@ import re
 
 coock_book_list = []
 names = []
+test = {}
 lol = []
-coock_book = {}
 ingredients = []
 
 
 with open('recipes.txt') as file:
-
     for i, line in enumerate(file):
         line = line.strip()
         line = line.split("|")
         name = re.search(r'[a-яА-ЯёЁ]', str(line))
         space = re.search(r'\w', str(line))
-
-        if  name and len(line)==1:
+        if name and len(line)==1:
             names.append(line)
-
         elif name and len(line)>1:
             tmp = dict()
             tmp['ingridient_name'] = line[0]
@@ -27,25 +24,22 @@ with open('recipes.txt') as file:
             tmp['measure'] = line[2]
             coock_book_list.append(tmp)
             ingredients = coock_book_list[:]
-
-
-
-
-
-
-
         elif not space:
+            test = lol
             ingredients2 = ingredients[:]
             lol.append(ingredients2)
-
             ingredients.clear()
             coock_book_list.clear()
 
 
 
 
-pprint(names)
-pprint(lol)
+coock_book = list(zip(names, lol))
+
+
+
+
+pprint(coock_book)
 
 
 
